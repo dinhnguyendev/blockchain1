384739dfe71e19d54d6195e961605137f4217803
bstardev\dinh.nguyen <dinh.nguyen@bstarsolutions.com>
2022-09-26 16:06:55 +0700

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+// import Web3 from 'web3';
 import './App.css';
-
 function App() {
+  // const [web3api,setWeb3Api]=useState({
+  //   provider: null,
+  //   web3:null
+  // });
+  // useEffect(()=>{
+  //   const loadProvider=async ()=>{
+  //     const provider=null;
+  //     if(provider){
+  //       setWeb3Api({
+  //         web3: new Web3(provider),
+  //         provider
+  //       })
+  //     }else{
+  //       alert("error... connect metamask !!!")
+  //     }
+  //   }
+  // },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex">
+        Curent - view: <span className="text">10 eth</span>
+        <div className="">
+          <button className="button__donate">Donate</button>
+          <button className="button__withdraw">Withdraw</button>
+        </div>
+      </div>
     </div>
   );
 }
